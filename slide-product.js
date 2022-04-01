7af03b7d8f5f1b348261972f97bed75a4c11e408
AmrElbehery <jjsjjsjhshjs@gmail.com>
2022-04-01 17:45:58 +0200

    All files for Sharkny company

---
+++ slide-product.js
@@ -0,0 +1,41 @@
+// Initialize Swiper
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// #################
+
+$(document).ready(function () {
+  $(".customer-logos").slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        setting: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 520,
+        setting: {
+          slidesToShow: 3,
+        },
+      },
+    ],
+  });
+});
